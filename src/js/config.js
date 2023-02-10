d061cb5d859e312c86cad16a9d85f5338c661820
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2023-02-11 01:32:36 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd29ccd67-60c0-45fa-86ff-af8221b0aa34';
+export const MODAL_CLOSE_SEC = 2.5;
